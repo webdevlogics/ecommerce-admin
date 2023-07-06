a16e04be4310d51c2374c09b9251590e9338d281
Michael S Wong <webdevlogics77@gmail.com>
2023-07-06 10:25:23 -0700

    added shadcn ui

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <div>
+      <p>Hello Admin Dashboard</p>
+      <Button size='default' variant='destructive'>Here is the Button</Button>
+    </div>
+  )
+}
