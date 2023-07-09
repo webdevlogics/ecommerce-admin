e701bf7d97dca75152d6d0196ec2f4d2200dc77d
Michael S Wong <webdevlogics77@gmail.com>
2023-07-09 08:31:16 -0700

    added prisma planetscale setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,32 @@
-import { Inter } from 'next/font/google'
-import { ClerkProvider } from '@clerk/nextjs'
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
-import { ModalProvider } from '@/providers/modal-provider'
+import { ModalProvider } from "@/providers/modal-provider";
+import { ToasterProvider } from "@/providers/toast-provider";
 
-import './globals.css'
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Admin Dashboard',
-  description: 'Admin Dashboard',
-}
+  title: "Admin Dashboard",
+  description: "Admin Dashboard",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ToasterProvider />
           <ModalProvider />
           {children}
         </body>
       </html>
     </ClerkProvider>
-  )
+  );
 }
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,37 +1,58 @@
-'use client'
+"use client";
 
-import * as z from "zod"
-import { useForm } from "react-hook-form"
-import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
-import { useStoreModal } from "@/hooks/use-store-modal"
-import { Modal } from "@/components/ui/modal"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Modal } from "@/components/ui/modal";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 const formSchema = z.object({
-  name: z.string().min(1)
-})
+  name: z.string().min(1),
+});
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
 
+  const [loading, setLoading] = useState(false);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      name: ""
-    }
-  })
+      name: "",
+    },
+  });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    console.log(values)
-    // TODO: Create Store
-  }
+    try {
+      setLoading(true);
+
+      const response = await axios.post("/api/stores", values);
+
+      toast.success("Store created.");
+    } catch (error) {
+      toast.error("Something went wrong.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Modal
-      title='Create Store'
+      title="Create Store"
       description="Add a new Store to manage products and categories"
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
@@ -42,12 +63,16 @@ export const StoreModal = () => {
             <form onSubmit={form.handleSubmit(onSubmit)}>
               <FormField
                 control={form.control}
-                name='name'
+                name="name"
                 render={({ field }) => (
                   <FormItem>
                     <FormLabel>Name</FormLabel>
                     <FormControl>
-                      <Input placeholder="E-Commerce" {...field} />
+                      <Input
+                        disabled={loading}
+                        placeholder="E-Commerce"
+                        {...field}
+                      />
                     </FormControl>
                     <FormMessage />
                   </FormItem>
@@ -55,18 +80,20 @@ export const StoreModal = () => {
               />
               <div className="pt-6 space-x-2 flex items-center justify-end w-full">
                 <Button
-                  variant='outline'
+                  disabled={loading}
+                  variant="outline"
                   onClick={storeModal.onClose}
-                >Cancel
+                >
+                  Cancel
                 </Button>
-                <Button type="submit">Continue</Button>
-
+                <Button disabled={loading} type="submit">
+                  Continue
+                </Button>
               </div>
-
             </form>
           </Form>
         </div>
       </div>
     </Modal>
-  )
-}
+  );
+};
